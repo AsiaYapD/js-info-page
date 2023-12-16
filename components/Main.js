5b234517cff06122ff3b8a9640baf594bc04c9b6
Asia Yap-Dubois <asiayapd@gmail.com>
2023-12-15 22:19:08 -0500

    Added documents to the file

---
+++ components/Main.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Main () {
+    return (
+        <div>
+            <h1>hello world</h1>
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function () {
+    return (
+        <nav>
+        <img src="../imgages/reactjs-icon.png"/>
+        <h3>content </h3>
+        <h4>content </h4>
+    </nav>
+    )
+}
